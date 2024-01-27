63c077e0230c74b8473a53828821986a24b64e49
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-01-27 19:09:52 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header';
 import Shop from './Component/Shop/Shop';
+import { Route, Routes } from 'react-router-dom';
+import Orders from './Component/Orders/Orders';
+import Inventory from './Component/Inventory/Inventory';
 
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+        <Routes>
+          <Route path='/' element={<Shop></Shop>}></Route>
+          <Route path='/shopping' element={<Shop></Shop>}></Route>
+          <Route path='/orders' element={<Orders></Orders>}></Route>
+          <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        </Routes>
     </div>
   );
 }
